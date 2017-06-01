37e57d9412555c3e4f8ab97a635037b7f817bb1a
habibcs <habib.ahmed@gmail.com>
2017-06-01 13:13:14 +0200

    integrated weather api

--- messages/index.js
+++ messages/index.js
@@ -1,8 +1,5 @@
 /*-----------------------------------------------------------------------------
-This template demonstrates how to use an IntentDialog with a LuisRecognizer to add 
-natural language support to a bot. 
-For a complete walkthrough of creating this type of bot see the article at
-http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+
 -----------------------------------------------------------------------------*/
 // @ts-check 
 
@@ -10,6 +7,7 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
+var weatherClient = require('./wunderground');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
@@ -35,47 +33,57 @@ const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-/*
-.matches('None', (session, args) => {
-    session.send('This is None intent, You said: \'%s\'', session.message.text)    
-})
-//*/
+    /*
+    .matches('None', (session, args) => {
+        session.send('This is None intent, You said: \'%s\'', session.message.text)    
+    })*/
 
-.matches('Weather', (session, args) => {
-    //session.send('You asked for weather : ' + JSON.stringify(args))
-    //var objUserIntention = JSON.parse(args);
-    var objUserIntention = args;
-    session.send('Weather for ' + objUserIntention.entities[0].entity + ' is great')
-    //console.log('Weather for ' + objUserIntention.entities[0].entity + ' is great');
-})
+    .matches('Weather', [
+        (session, args, next) => weatherParserFunction(session, args, next),
+        (session, results) => {
+            weatherClient.getCurrentWeather(results.response, (responseString) => {
+                session.send(responseString);
+            })
+        }
+    ])
 
-.matches('Greeting', (session, args) => {
-    session.send('Hello - Thanks for the greeting!')
-})
+    .matches('Greeting', (session, args) => {
+        session.send('Hello - Thanks for the greeting!')
+    })
 
-.matches('EndGreeting', (session, args) => {
-    if (session.message.text.toLowerCase().match(/have a good.*/))
-        session.send('You too, bye.');
-    else session.send('Have a good day! Bye.')
-})
+    .matches('EndGreeting', (session, args) => {
+        if (session.message.text.toLowerCase().match(/have a good.*/))
+            session.send('You too, bye.');
+        else session.send('Have a good day! Bye.')
+    })
 
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+    });
 
-bot.dialog('/', intents);    
+bot.dialog('/', intents);
 
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
-    server.listen(3978, function() {
+    server.listen(3978, function () {
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
 } else {
     module.exports = { default: connector.listen() }
 }
 
+function weatherParserFunction(session, args, next) {
+    //var locationEntity = builder.EntityRecognizer.findEntity(args.entities, 'Weather.Location'); //args.entities[0].entity;
+    var city = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.city');
+    var country = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.country');
+    if (city && city.entity != 'there') {
+        session.dialogData.searchType = 'weather';
+        session.send('Fetching the weather info for the location: \'%s\'', city.entity);
+        next({ response: city.entity + ', ' + country.entity });
+    }
+    else
+        session.send('You did not tell me which place do you want to know the weather for, What location?');
+        //builder.Prompts.text(session, 'You did not tell me which place do you want to know the weather for, What location?'); // => this way directly sends to the next in response function
+}
